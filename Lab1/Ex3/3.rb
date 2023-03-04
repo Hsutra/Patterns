fa79d96601d03a7fe859a65fc50fe0f99f29d4e6
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-03-04 13:20:08 +0300

    Update 3.rb

--- Lab1/Ex3/3.rb
+++ Lab1/Ex3/3.rb
@@ -19,18 +19,14 @@ def max(a)
 end
 
 def first_positive(a)
-	k = -1
+	k = 0
 	for i in 0..a.size
     	if(a[i] > 0)
     		k = i
     		break
     	end
     end
-    if(k == -1)
-    	return puts "Нету положительных элементов"
-    else
-    	return puts "Результат: Номер первого положительного элемента: #{k}"
-    end
+    return k
 end
 
 
@@ -43,7 +39,11 @@ if(ARGV[1] == "1")
 elsif(ARGV[1] == "2")
 	puts "Результат: Максимальный элемент массива: ", max(arr)
 elsif(ARGV[1] == "3")
-	first_positive(arr)
+	if(first_positive(arr) == 0)
+		puts "В массиве нету положительных элементов"
+	else
+		puts "Результат: Номер первого положительного элемента:", first_positive(arr)
+	end
 else 
 	puts "Результат: Чел, собирай утром рюкзачек и иди на алгебру"
 end
