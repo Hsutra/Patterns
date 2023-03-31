bb9f4ee53d26de136917daee7e61d83ac07f9024
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-03-31 21:49:31 +0300

    Create 1.1.rb

---
+++ Lab2/1.1.rb
@@ -0,0 +1,74 @@
+class Student 
+
+	def initialize(surname, name, father_name)
+		@id
+		@surname = surname
+		@name = name
+		@father_name = father_name
+		@phone
+		@telegram
+		@email
+		@git
+	end
+	
+	def getid
+		@id
+	end
+
+	def setid(id)
+		@id = id
+	end
+	
+	def getsurname
+		@surname
+	end
+
+	def setsurname(surname)
+		@surname = surname
+	end
+	
+	def getname
+		@name
+	end
+
+	def setname(mame)
+		@name = name
+	end
+	
+	def getlastName
+		@lastName
+	end
+
+	def setlastName(lastName)
+		@lastName = lastName
+	end
+	
+	def getphone
+		@phone
+	end
+	def setphone(phone)
+		@phone = phone
+	end
+	
+	def gettelegram
+		@tme
+	end
+	def settelegram(telegram)
+		@telegram = telegram
+	end
+	
+	def getemail
+		@email
+	end
+
+	def setemail(email)
+		@email = email
+	end
+	
+	def getgit
+		@git
+	end
+
+	def setgit(git)
+		@git = git
+	end
