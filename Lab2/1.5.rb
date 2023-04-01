d22e286561f5772f5a7a63c612a23b628714c102
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-04-01 08:56:24 +0300

    Update 1.5.rb

--- Lab2/1.5.rb
+++ Lab2/1.5.rb
@@ -2,21 +2,21 @@ class Student
 
 	attr_accessor :id, :surname, :name, :father_name, :phone, :telegram, :email, :git
 
-	def initialize(id:nil, surname:, name:, father_name:, phone:nil, telegram:nil, email:nil, git:nil)
-		self.id
+	def initialize(surname, name, father_name, id:nil, phone:nil, telegram:nil, email:nil, git:nil)
+		self.id=id
 		self.surname = surname
 		self.name = name
 		self.father_name = father_name
-		self.phone
-		self.telegram
-		self.email
-		self.git
+		self.phone=phone
+		self.telegram=telegram
+		self.email=email
+		self.git=git
 	end
 	 
 	def to_s
     	"ID: #{@id}, Фамилия: #{@surname}, Имя: #{@name}, Отчество: #{@father_name}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}\n"
   	end
-
 end
 
-student1 = Student.new(surname:"Абуюков", name:"Эуфар", father_name:"Маратович")
+student1 = Student.new("Абуюков","Эуфар","Маратович")
+puts student1
