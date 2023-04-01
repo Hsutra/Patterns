0d3e65f607c097be02fd869ca85747d63c3526de
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-04-01 19:52:58 +0300

    Update 1.10.rb

--- Lab2/1.10.rb
+++ Lab2/1.10.rb
@@ -1,121 +1,102 @@
 class Student
 
-	attr_accessor :id, :surname, :name, :father_name, :phone, :telegram, :email, :git
+  attr_accessor :id, :surname, :name, :father_name, :phone, :telegram, :email, :git
 
-	def initialize(surname:, name:, father_name:, id:nil, phone:nil, telegram:nil, email:nil, git:nil)
-		
-		if(id != nil)
-			if(id.class != Integer)
-				raise ArgumentError, "Формат индентификационного номера неверный!"
-			end
-		end
+  def initialize(surname:, name:, father_name:, id:nil, phone:nil, telegram:nil, email:nil, git:nil)
 
-		if(surname == nil or name == nil or father_name == nil)
-			raise ArgumentError, "ФИО не задано!"
-		end
+    self.id=id
+    self.surname = surname
+    self.name = name
+    self.father_name = father_name
+    self.phone=phone
+    self.telegram=telegram
+    self.email=email
+    self.git=git
+  end
+   
+  def to_s
+      "ID: #{@id}, Фамилия: #{@surname}, Имя: #{@name}, Отчество: #{@father_name}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}\n"
+  end
 
-		if(phone != nil)
-			if (is_right_phone(phone) == false)
-				raise ArgumentError, "Формат номера телефона неверный!"
-			end
-		end
-		
-		if(telegram != nil)
-			if(is_right_telegram == false)
-				raise ArgumentError, "Формат ссылки на телеграм неверный!"
-			end
-		end
-				
-		if(email != nil)
-			if(is_right_email == false)
-				raise ArgumentError, "Формат почты неверный!"
-			end
-		end
+  def self.is_right_ID(id)
+    id.match /^[0-9]$/
+  end
 
-		if(git != nil)
-			if(is_right_git == false)
-				raise ArgumentError, "Формат гита неверный!"
-			end
-		end
+  def id=(id)
+    raise ArgumentError,"Формат индентификационного номера неверный!" if !id.nil? && !Student.is_right_ID(id)
+  end
 
-		self.id=id
-		self.surname = surname
-		self.name = name
-		self.father_name = father_name
-		self.phone=phone
-		self.telegram=telegram
-		self.email=email
-		self.git=git
-	end
-	 
-	def to_s
-    	"ID: #{@id}, Фамилия: #{@surname}, Имя: #{@name}, Отчество: #{@father_name}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}\n"
-  	end
+  def self.is_right_FIO(fio)
+    fio.match /^[ЁёА-я]+$/ #только символы внутри регулярки
+  end
 
-  	def is_right_phone(phone)
-  		if phone =~ /^((8|\+7))\d{10}$/ then
-  			return true
-  		else 
-  			return false
-  		end
-	end
-	
-	def is_right_telegram(telegram)
-		if telegram =~ /^https:\/\/t.me\//
-			return true
-		else 
-			return false
-		end
-	end
+  def surname=(surname)
+    raise ArgumentError,"Формат фамилии неверный!" if !Student.is_right_FIO(surname)
+    @surname=surname
+  end
+  
+  def name=(name)
+    raise ArgumentError,"Формат имени неверный!" if !Student.is_right_FIO(name)
+    @name=name
+  end
+  
+  def father_name=(father_name)
+    raise ArgumentError,"Формат отчества неверный!" if !Student.is_right_FIO(father_name)
+    @father_name=father_name
+  end
 
-	def is_right_email(email)
-		if email =~ /(@gmail\.com)$/
-			return true
-		else 
-			return false
-		end
-	end
+  def self.is_right_phone(phone)
+    phone.match /^((8|\+7))\d{10}+$/ 
+  end
 
-	def is_right_git(git)
-		if git =~ /^https:\/\/github\.com/
-			return true
-		else
-			return false
-		end
-	end
+  def phone=(phone)
+    raise ArgumentError,"Формат номера телефона неверный!" if !phone.nil? && !Student.is_right_phone(phone)
+    @phone = phone
+  end
+  
+  def self.is_right_telegram(telegram)
+    telegram.match /^https:\/\/t.me\// 
+  end
 
-	def validate_contacts()
-		if self.phone or self.email or self.telegram
-			return true
-		else
-			return false
-		end
-	end
+  def telegram=(telegram)
+    raise ArgumentError,"Формат телеграма неверный!" if !telegram.nil? && !Student.is_right_telegram(telegram)
+    @telegram = telegram
+  end
+  
+  def self.is_right_email(email)
+    email.match /(@gmail\.com)$/
+  end
 
-	def validate_git()
-		if self.git
-			return true
-		else 
-			return false
-		end
-	end
+  def email=(email)
+    raise ArgumentError,"Формат почты неверный!" if !email.nil? && !Student.is_right_email(email)
+    @email = email
+  end  
 
-	def validate()
-		if validate_git or validate_contacts
-			return true
-		else 
-			return false
-		end
-	end
+  def self.is_right_git(git)
+    git.match /^https:\/\/github\.com/
+  end
 
-	def set_contacts(phone:nil, telegram:nil, email:nil)
+  def git=(git)
+    raise ArgumentError,"Формат гита неверный!" if !git.nil? && !Student.is_right_git(git)
+    @telegram = telegram
+  end
 
-		self.phone = phone
-		self.telegram = telegram
-		self.email = email
+  def validate_git()
+    @git!=nil
+  end
 
-	end
+  def validate_contacts()
+    @phone!=nil or @telegram!=nil or @email!=nil
+  end
+
+  def validate()
+    validate_contacts && validate_git
+  end
+
+  def set_contacts(phone:nil, telegram:nil, email:nil)
+    self.phone = phone if(phone != nil)
+    self.telegram = telegram if(telegram) != nil)
+    self.email = email if(email != nil)
+  end
 
 end
-
-
