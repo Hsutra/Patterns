8e2027fac818ab60bfc58634b037a564d08171c1
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-05-13 13:43:55 +0300

    Create 3.5.rb

---
+++ Lab2/Ex3/3.5.rb
@@ -0,0 +1,25 @@
+load("RubyLabs/Lab2/Ex3/Data_List.rb")
+load("RubyLabs/Lab2/Ex3/Data_table.rb")
+
+class Data_list_student_short < Data_list
+
+    def self.get_names
+        @list[1..-1]
+    end
+  
+    def get_date
+        result = []
+        id = 0
+        list.each do |obj|
+            row = []
+            row << id
+            row.push(*get_names(obj))
+            result << row
+            id += 1
+        end
+        Data_table.new(result)
+    end
+
+end
+
+
