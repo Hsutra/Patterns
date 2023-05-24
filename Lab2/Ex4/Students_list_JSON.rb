0c621bd0f31caf300f3b96b8d745e28bd1dc476e
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-05-24 18:39:14 +0300

    Update Students_list_JSON.rb

--- Lab2/Ex4/Students_list_JSON.rb
+++ Lab2/Ex4/Students_list_JSON.rb
@@ -1,1 +1,17 @@
+load("C:/Users/artus/VsCodeProjects/RubyLabs/Lab2/Ex4/Student_list_strategy.rb")
+require 'json'
 
+class StudentListJson<StudentListStrategy
+  public_class_method :new
+
+  #получение массива хэшей из строки
+  def list_hash_from_str(str)
+    JSON.parse(str,  {symbolize_names: true })
+  end
+
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    JSON.generate(list_hash)
+  end
+
+end
