aa67cee986b06321d30d37d75fc2aec7555fae01
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-05-24 10:29:00 +0300

    Update 3.6.rb

--- Lab2/Ex3/3.6.rb
+++ Lab2/Ex3/3.6.rb
@@ -1,31 +1,37 @@
-#Паттерн паттерн(шаблон)
-class Car
-  def drive
-    puts "Driving a car"
-  end
-end
-
-class Motorcycle
-  def drive
-    puts "Riding a motorcycle"
-  end
-end
-
-class VehicleFactory
-  def create_vehicle(type)
-    case type
-    when :car
-      Car.new
-    when :motorcycle
-      Motorcycle.new
-    else
-      raise "Unknown vehicle type: #{type}"
+class AbstractClass
+    def template_method
+      step_1
+      step_2
+      step_3
+    end
+  
+    def step_1
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+  
+    def step_2
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+  
+    def step_3
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
     end
   end
-end
-
-factory = VehicleFactory.new
-vehicle1 = factory.create_vehicle(:car)
-vehicle1.drive
-vehicle2 = factory.create_vehicle(:motorcycle)
-vehicle2.drive
+  
+  class ConcreteClass < AbstractClass
+    def step_1
+      puts "Executing step 1"
+    end
+  
+    def step_2
+      puts "Executing step 2"
+    end
+  
+    def step_3
+      puts "Executing step 3"
+    end
+  end
+  
+  # Использование
+  concrete_object = ConcreteClass.new
+  concrete_object.template_method
