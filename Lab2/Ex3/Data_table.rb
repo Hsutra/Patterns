6d5030b790f5232e5e2decb0bd81f35f4d2274c8
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-05-24 15:56:47 +0300

    Update Data_table.rb

--- Lab2/Ex3/Data_table.rb
+++ Lab2/Ex3/Data_table.rb
@@ -1,22 +1,27 @@
 class Data_table
 
-    attr_reader :table
+    attr_reader :row_number, :column_number
   
     def initialize(table)
-        @table = table
+      @table = table
+      @row_number = table.size
+      @column_number = max_size_column
     end
-
-    def get_value(row, col)
-        @table[row][col]
+  
+    #получить элемент по номеру столбца и строки
+    def get_item(row,col)
+      return nil if row>=@row_number||col>=@column_number
+      table[row][col]
     end
-    
-    def num_columns
-        @table[0].length
+  
+    private
+    attr_accessor :table
+    attr_writer :row_number, :column_number
+  
+    #максимальный размер столбца
+    def max_size_column
+      max_size = 0
+      table.each{|elem| max_size=elem.size if elem.size>max_size}
+      max_size
     end
-
-    def num_rows
-        @table.length
-    end
-
-end
-
+  end
