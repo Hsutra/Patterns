9466b76278fd243eef6dcb2691acc2025796c413
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-07-04 14:32:51 +0300

    Delete Lab2 directory

--- Lab2/Ex1/1.2/main.rb
+++
@@ -1,6 +0,0 @@
-load('test.rb')
-
-student1 = Student.new("Абуюков", "Эуфар", "Маратович")
-student2 = Student.new("Епифанец", "Ничеговолод", "Александроваич")
-
-puts student1, student2
--- Lab2/Ex4/Student_list_strategy.rb
+++
@@ -1,8 +0,0 @@
-class StudentListStrategy  
-  private_class_method :new
-
-  def list_hash_from_str(str); end
-  
-  def list_hash_to_str(list_hash); end
-  
-end
