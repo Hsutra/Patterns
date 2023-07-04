47e00759fc6272f2b7131a0cebde56042eff41e5
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-07-04 14:33:03 +0300

    Delete Lab3/3.1 directory

--- Lab3/3.1/sql/create_student_short_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS students (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    fullname    text not null,
-    git         text null,
-    contact     text null
-);
--- Lab3/3.1/sql/insert_student_short_data.sql
+++
@@ -1,3 +0,0 @@
-insert into students(fullname, git, contact) values ('Иванов П.Е.', '@fdhs', '+898854431254');
-insert into students(fullname, git, contact) values ('Иванов П.А.', '@jyrfd', '+898854487654');
-insert into students(fullname, git, contact) values ('Петров М.Ф.', '@gfjiowhj', '+898854098544');
